a4132578ed3b1b74ad4b80baf770663a2bfde52d
Feetvi <razer-key@mail.ru>
2018-02-25 20:13:19 +0300

    my solution

--- src/index.js
+++ src/index.js
@@ -1,30 +1,53 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.arr = []; // создаём "store" внутри instance
+    // функция сортировки по возрастанию
+    this.incFunc = function (a, b) { 
+      return a - b;
+    }
   }
-
+  // метод добавления элементов в "store"
   add(element) {
-    // your implementation
+    return this.arr.push(element);
   }
-
+  // возвращаем указанный по индексу элемент
   at(index) {
-    // your implementation
+    return this.arr[index];
   }
-
+  // возвращаем количество элементов в "store"
   get length() {
-    // your implementation
+    return this.arr.length;
   }
-
+  // возвращаем "store"
   toArray() {
-    // your implementation
+    return this.arr;
   }
-
+  // принимаем индексы существующих элементов и сортиуем только их
   sort(indices) {
-    // your implementation
+    
+    var tempArr = [];
+    // сортировка по возрастанию массива индексов
+    indices.sort(function (a, b) {
+      return a - b;
+    });
+
+    // заносим нужные для сортировки элементы во временный массив
+    for (var i = 0; i < indices.length; i++) {
+      tempArr.push(this.arr[indices[i]]);
+    }
+
+    
+    // сортировка
+    tempArr.sort(this.incFunc);
+    // возвращаем элементы обратно в "store"
+    for (var i = 0; i < indices.length; i++) {
+      this.arr[indices[i]] = tempArr[i];
+    }
   }
 
+  // принимаем переданную функцию для порядка сортировки
   setComparator(compareFunction) {
-    // your implementation
+    this.incFunc = compareFunction;
   }
 }
 
